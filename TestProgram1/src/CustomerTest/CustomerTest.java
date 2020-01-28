699e0a02af57f62328b685d59cd4b6e54c8858e6
sriharsha <sriharsha@sriharsha-PC>
2020-01-28 23:01:47 +0530

    new msg

---
+++ TestProgram1/src/CustomerTest/CustomerTest.java
@@ -0,0 +1,10 @@
+package CustomerTest;
+
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("customer test");
+	}
+
+}
